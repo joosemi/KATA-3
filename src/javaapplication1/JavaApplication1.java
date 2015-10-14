b473fdbb90bd6c177d622214619e827f31d6cf8b
usuario <usuario@10.22.143.32>
2015-10-14 10:21:56 +0100

    Tercera versión

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -3,28 +3,17 @@ package javaapplication1;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class JavaApplication1 {
-    private static String pathname;
-
     public static void main(String[] args) throws FileNotFoundException {
-        
         Histogram<String> histograma = new Histogram<>();
         String pathName = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\JavaApplication1\\emails.txt";
-        try {
-            BufferedReader fileIn = new BufferedReader(new FileReader(pathName));
-            String mail;
-            while((mail=fileIn.readLine()) != null) {
-                if(!mail.contains("@"))
-                    continue;
-                histograma.increment(mail.split("@")[1]);
-            }
-                
-                
-        } catch (Exception e) {
-            System.out.println("El fichero no existe " + e);
+        MailReader dominios = new MailReader(pathName);
+        ArrayList<String> domis = dominios.getDominiosArray();
+        for (int i = 0; i < domis.size(); i++) {
+            histograma.increment(domis.get(i));
         }
-    
         HistogramDisplay histo = new HistogramDisplay(histograma);
         histo.execute();
     }
