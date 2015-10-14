a996a95f9b680e5789eafb9bbdaab16c096f6105
usuario <usuario@10.22.143.32>
2015-10-14 10:29:23 +0100

    Cuarta versión

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -7,13 +7,10 @@ import java.util.ArrayList;
 
 public class JavaApplication1 {
     public static void main(String[] args) throws FileNotFoundException {
-        Histogram<String> histograma = new Histogram<>();
         String pathName = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\JavaApplication1\\emails.txt";
         MailReader dominios = new MailReader(pathName);
         ArrayList<String> domis = dominios.getDominiosArray();
-        for (int i = 0; i < domis.size(); i++) {
-            histograma.increment(domis.get(i));
-        }
+        Histogram<String> histograma = DominiosHistogramBuilder.execute(domis);
         HistogramDisplay histo = new HistogramDisplay(histograma);
         histo.execute();
     }
